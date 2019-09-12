88fe95d153dae6f3b3b27a880ea3ff9e77bc9447
Robert van Braam <robert@omniaretail.com>
2019-09-12 21:37:15 +0200

    Added webhost to project with signal R

---
+++ src/SustainableVendingMachine.Domain/UseCases/Results/Result.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SustainableVendingMachine.Domain.UseCases.Results
+{
+    public class Result
+    {
+        private readonly bool _isSuccessful;
+
+        public Result(string message, bool isSuccessful = true)
+        {
+            Message = message;
+            _isSuccessful = isSuccessful;
+        }
+
+        public bool HasFailed => !_isSuccessful;
+        public string Message { get; }
+    }
+}
---
+++ src/SustainableVendingMachine.Host.Web/Hubs/VendingMachineHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SustainableVendingMachine.Host.Web.Hubs
+{
+    public class VendingMachineHub : Hub
+    {
+        public async Task SendDisplayMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveDisplayMessage", message);
+        }
+    }
+}
